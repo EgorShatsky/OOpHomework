51fd0642ebf642f79af7d84f0526c8d789f02e91
EgorShatsky <icewizorcr@gmail.com>
2020-09-22 20:38:04 +0300

    Successful execution of homework

---
+++ Task09.java
@@ -0,0 +1,39 @@
+package lesson01.part1;
+
+public class Task09 {
+
+    /**
+     * Напиши код метода sumDigitsInNumber(int number). Метод на вход принимает целое трехзначное число. Нужно посчитать сумму цифр этого числа, и вернуть эту сумму.
+     * <p>
+     * Пример:
+     * Метод sumDigitsInNumber вызывается с параметром 546.
+     * <p>
+     * Пример вывода:
+     * 15
+     * <p>
+     * <p>
+     * Требования:
+     * 1. Программа не должна считывать данные с клавиатуры.
+     * 2. Метод sumDigitsInNumber(int) должен быть публичным и статическим.
+     * 3. Метод sumDigitsInNumber должен возвращать значение типа int.
+     * 4. Метод sumDigitsInNumber не должен ничего выводить на экран.
+     * 5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
+     * 6. Что будет если написать sout и нажать tab?
+     */
+
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(999));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+       int a,b,c;
+       a=number;
+       b=number%100;
+       c=(number%100)%10;
+       a=number/100;
+       b=(number%100)/10;
+       number=a+b+c;
+
+       return number;
+    }
+}
